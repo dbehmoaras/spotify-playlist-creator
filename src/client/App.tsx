f46826c543768f86b7a1f4e500959ae0619fe386
dbehmoaras@gmail.com <dbehmoaras@gmail.com>
2021-08-12 13:22:15 -0400

    basic rendering of a playlist using a song interface and passing it through the useState hook

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 
+import Playlist from './containers/Playlist';
+
 function App() {
   return(
-    <h1 id="colorTestDiv">
-      HEY IT'S A DIV
-    </h1>
+    <div>
+      <h1 id="colorTestDiv">
+        HEY IT'S A DIV
+      </h1>
+      <Playlist />
+    </div>
   )
 }
 
---
+++ src/client/containers/Playlist.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+declare function require(name:string);
+
+
+interface Song {
+	Title: string,
+	Artist: string,
+}
+
+const songList: Song[] = [
+	{
+		"Title": "Time",
+		"Artist": "Hans Zimmer",
+	},
+	{
+		"Title": "From Past To Present",
+		"Artist": "Jeremy Soule"
+	}
+];
+
+function Playlist () {
+
+	const [songs, setSongs] = useState(songList)
+
+	return(
+		<div>
+			<div className='playlist-container'>
+				{songs.map((ele) => {
+					return (<div>
+						<span>{ele.Title}</span>
+						<span>{ele.Artist}</span>
+					</div>)
+				})}
+			</div>
+		</div>
+	)
+}
+
+export default Playlist;
