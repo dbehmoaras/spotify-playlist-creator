013f7a2a727944ac27d9b47b103f499c2e126ae8
dbehmoaras@gmail.com <dbehmoaras@gmail.com>
2021-09-02 12:23:31 -0400

    begin testing venture with spotify playlist creator

--- __test__/App.test.tsx
+++ __test__/App.test.tsx
@@ -4,13 +4,6 @@ import App from "../src/client/App";
 import FunctionButton from "../src/client/components/FunctionButton";
 import "@testing-library/jest-dom/extend-expect"
 
-// test("FuncButt", () => {
-//   render(<FunctionButton id="funcButtTest" title="funcButtTest" func={(mock)=> console.log(mock)} name={"FUNC_NAME_TEST"} data={{data: "mockData"}}/>);
-//   const comp = screen.getByText('FUNC_NAME_TEST');
-//   console.log(comp);
-//   expect(comp).toBeInTheDocument();
-// });
-
 test('header renders with correct text', () => {
   //can destructure getByTestId
   const component = render(<App />);
--- __test__/sum.test.ts
+++
@@ -1,5 +0,0 @@
-const sumTest = require('./../src/sum');
-
-test('sumTest', () => {
-	expect(sumTest(1,2)).toBe(3);
-})
